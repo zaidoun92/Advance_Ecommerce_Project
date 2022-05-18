6a55110a78dd936428239eb96c576002e798e148
zaidoun92 <27ew75ma@gmail.com>
2022-05-18 11:07:38 +0300

    add select one image and multi image to the product page

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -277,11 +277,12 @@
                                 <div class="form-group">
                                     <h5>Main Thambnail <span class="text-danger">*</span></h5>
                                     <div class="controls">
-                                        <input type="file" name="product_thambnail" class="form-control">
+                                        <input type="file" name="product_thambnail" class="form-control" onchange="mainThamUrl(this)">
                                     </div>
                                     @error('product_thambnail')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
+                                    <img src="" id="mainThmb" alt="">
                                 </div>
                             </div> {{-- end col --}}
 
@@ -289,11 +290,13 @@
                                 <div class="form-group">
                                     <h5>Multiple Image <span class="text-danger">*</span></h5>
                                     <div class="controls">
-                                        <input type="file" name="multi_img[]" class="form-control">
+                                        <input type="file" name="multi_img[]" class="form-control" multiple="" id="multiImg">
                                     </div>
                                     @error('multi_img')
                                         <span class="text-danger">{{ $message }}</span>
                                     @enderror
+
+                                    <div class="row" id="preview_img"></div>
                                 </div>
                             </div> {{-- end col --}}
                         </div> {{-- end Sex Row --}}
@@ -423,6 +426,7 @@
                     type:"GET",
                     dataType:"json",
                     success:function(data) {
+                        $('select[name="subsubcategory_id"]').html('');
                         var d =$('select[name="subcategory_id"]').empty();
                         $.each(data, function(key, value){
                             $('select[name="subcategory_id"]').append('<option value="'+ value.id +'">'+ value.subcategory_name_en +'</option>');
@@ -460,6 +464,58 @@
                 alert('danger');
             }
         });
+    });
+</script>
+
+
+<script type="text/javascript">
+    function mainThamUrl(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#mainThmb').attr('src',e.target.result).width(80).height(80);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
+
+
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#multiImg').on('change', function() {
+            // check File API supported browser
+            if (window.File && window.FileReader && window.FileList && window.Blob) {
+
+                var data = $(this)[0].files; // this is file data
+
+                $.each(data, function(index, file) { // loop though each file
+
+                    if (/(\.|\/)(gif|jpe?g|png)$/i.test(file.type)) { // check supported file type
+
+                        var fRead = new FileReader();
+
+                        fRead.onload = (function(file) {
+
+                            return function(e) {
+
+                                var img = $('<img/>').addClass('thumb').attr('src', e.target.result).width(80).height(80); // create image element
+
+                                $('#preview_img').append(img); // append image to output element
+                            };
+                        })(file);
+
+                        fRead.readAsDataURL(file);
+
+                    }
+
+                });
+
+            } else {
+                alert("Your browser doesn't support File API!");
+            }
+        })
     });
 </script>
 
