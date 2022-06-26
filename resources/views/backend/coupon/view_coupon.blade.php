d65ee6ccec6f7d2bfbf5f8d0b7c949efab123d27
zaidoun92 <27ew75ma@gmail.com>
2022-06-26 23:32:56 +0300

    add User Online or last seen function to admin panel

--- resources/views/backend/coupon/view_coupon.blade.php
+++ resources/views/backend/coupon/view_coupon.blade.php
@@ -14,7 +14,7 @@
 
 			 <div class="box">
 				<div class="box-header with-border">
-				  <h3 class="box-title">Coupon List</h3>
+				  <h3 class="box-title">Coupon List <span class="badge badge-pill badge-danger">{{ count($coupon)}}</span></h3>
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body">
